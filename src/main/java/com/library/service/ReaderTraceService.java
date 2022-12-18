da94e790e0d99fd2cab9265305ead22e4a392f66
yiwen.chen <2467325644@qq.com>
2022-12-18 23:35:18 +0800

    add ReaderTraceService.java新增[埋点]跟踪检索过程功能

---
+++ src/main/java/com/library/service/ReaderTraceService.java
@@ -0,0 +1,15 @@
+package com.library.service;
+
+import com.library.dao.ReaderTraceDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReaderTraceService {
+    @Autowired
+    private ReaderTraceDao readerTraceDao;
+
+    public boolean addTrace(long reader_id, String behavior, String detail) {
+        return readerTraceDao.addTrace(reader_id, behavior, detail)>0;
+    }
+}
