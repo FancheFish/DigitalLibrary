f200bcb2b8a3d737b4d7f138e7f21e9645ab9159
yiwen.chen <2467325644@qq.com>
2022-12-18 23:32:36 +0800

    add ReaderTraceDao.java

---
+++ src/main/java/com/library/dao/ReaderTraceDao.java
@@ -0,0 +1,25 @@
+package com.library.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class ReaderTraceDao {
+
+    @Resource
+    private SqlSessionTemplate sqlSessionTemplate;
+
+    private final static String NAMESPACE = "com.library.dao.ReaderTraceDao.";
+
+    public int addTrace(final long reader_id, final String behavior, final String detail) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("reader_id", reader_id);
+        map.put("behavior", behavior);
+        map.put("detail", detail);
+        return sqlSessionTemplate.insert(NAMESPACE + "addTrace", map);
+    }
+}
