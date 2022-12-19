862b50e630cc31d22327db45aa3a92f34cd44344
yiwen.chen@ximalaya.com <yiwen.chen@ximalaya.com>
2022-12-19 13:46:55 +0800

    update ReaderTraceController.java

--- src/main/java/com/library/controller/ReaderTraceController.java
+++ src/main/java/com/library/controller/ReaderTraceController.java
@@ -1,7 +1,23 @@
 package com.library.controller;
 
+import com.library.service.ReaderTraceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ReaderTraceController {
+
+    @Autowired
+    private ReaderTraceService readerTraceService;
+
+    @RequestMapping("/get_all_traces.html")
+    public ModelAndView lendList() {
+        ModelAndView modelAndView = new ModelAndView("admin_reader_trace");
+        modelAndView.addObject("trace_list", readerTraceService.getAllTraces());
+        return modelAndView;
+    }
 }
