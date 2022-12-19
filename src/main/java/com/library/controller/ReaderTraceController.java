b93471526691d7a21db57c89331cffb4960d31f9
yiwen.chen <2467325644@qq.com>
2022-12-19 15:10:50 +0800

    update ReaderTraceController.java 新增管理员端埋点系统页面

--- src/main/java/com/library/controller/ReaderTraceController.java
+++ src/main/java/com/library/controller/ReaderTraceController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ReaderTraceController {
